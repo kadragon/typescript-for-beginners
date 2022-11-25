3c9a91a9d6e7cb7c72dea1337dc2d23fdb044af0
kangdonguk <kangdongouk@gmail.com>
2022-11-25 13:51:50 +0900

    end

---
+++ 05/typechain/src/index.ts
@@ -0,0 +1,59 @@
+import { timeStamp } from "console";
+import crypto from "crypto";
+import { brotliCompress } from "zlib";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash(): string {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Seconde one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+// hacked 예시
+blockchain.getBlocks().push(new Block("XXX", 99999, "You Hacked!"));
+
+console.log(blockchain.getBlocks());
--- typechain/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { init, exit } from "./myPackage";
-
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
